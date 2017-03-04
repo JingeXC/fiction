91888b459f03670cee908cbdd6704bc3d00d2d30
JingeXC <cbb467cp01@163.com>
2017-03-04 21:32:43 +0800

    实现登录跳转

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,12 +17,15 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
-});
+})
+.controller('LoginCtrl',function($scope){
+  $scope.goback=function(){
+    window.history.back();
+  }
+})
+;
