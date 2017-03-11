19ba559cd6a9f4f902210ca4c80f95d0d5926af3
JingeXC <cbb467cp01@163.com>
2017-03-11 14:00:48 +0800

    添加假数据

--- www/js/services.js
+++ www/js/services.js
@@ -2,6 +2,7 @@ angular.module('starter.services', [])
 
 .factory('httpUrlService',function(){
   return{
-    httpUrl:'http://182.254.216.136'
+    //httpUrl:'http://182.254.216.136'
+    httpUrl:'falseData'
   }
 });
